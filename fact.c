e06d5761f011de24fb734f6bf3999a5f4181c33b
suraj25bcon2630-coder <suraj.25bcon2630@jecrcu.edu.in>
2026-09-07 09:23:54 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -3,14 +3,16 @@
 int main() {
     int n, i;
     long long fact = 1;
-
+// Take the number from the user
     printf("Enter a number: ");
     scanf("%d", &n);
-
+    
+// Calculate factorial using a loop
     for (i = 1; i <= n; i++) {
         fact = fact * i;
     }
-
+    
+ // Display the calculated factorial
     printf("Factorial = %lld\n", fact);
 
     return 0;
